898d5f5fa1ac4330bed469333ed4aecb84c45b22
Vibhutha Kumarage <vkartz@gmail.com>
2018-10-30 17:32:34 +0530

    Refactored App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PrimarySearchAppBar from './components/AppHeader';
+import CardsGrid from './components/CardsGrid';
+import NavigationTime from './components/NavigationTime';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <PrimarySearchAppBar/>
+        <NavigationTime/>
+        <CardsGrid/>
       </div>
     );
   }

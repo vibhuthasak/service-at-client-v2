7b855a60777926e8e846178c4b23a18941cd9206
Vibhutha Kumarage <vkartz@gmail.com>
2018-10-31 20:29:00 +0530

    Changed ActionsBar Component to Class Component

--- src/views/components/ActionsBar.js
+++ src/views/components/ActionsBar.js
@@ -2,8 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+import KeyboardArrowDown from '@material-ui/icons/KeyboardArrowDown';
+import ActionsBarOptions from '../components/ActionsBarOptions';
 
 const styles = {
   card: {
@@ -14,42 +16,61 @@ const styles = {
     marginLeft: 10,
     marginRight: 10
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)"
+  actionPanel: {
+    'marginTop': 14,
+    'marginLeft': 60,
+    'marginBottom': 16,
+    'color': 'white',
+    'fontSize': 'large',
+    'fontWeight': 500
   },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12,
+  button: {
+    marginLeft: 5
   }
 };
 
-function ActionsBar(props) {
-  const { classes } = props;
+class ActionsBar extends React.Component {
+  render() {
+    const { classes } = this.props;
 
-  // const headerColor = props.background;
-  return (
-    <Card className={classes.card}>
-      <CardContent>
-        <Grid container spacing={0} >
-          <Grid item xs={3}>
+    // const headerColor = props.background;
+    return (
+      <div>
+        <Card className={classes.card}>
+          <Grid container spacing={0} >
+            <Grid item xs={2} style={{ background: '#3f51b5', 'borderRadius': '4px'}}>
+              <div className={classes.actionPanel}>
+                Actions
+              </div>
+            </Grid>
+            <Grid item xs={10} style={{marginTop: '5px'}}>
+              <Button variant="contained" color="primary" className={classes.button}>
+                Create SR
+                <KeyboardArrowDown/>
+              </Button>
+              <Button variant="contained" color="primary" className={classes.button}>
+                Create FCR
+                <KeyboardArrowDown/>
+              </Button>
+              <Button variant="contained" color="primary" className={classes.button}>
+                Genie Portal
+                <KeyboardArrowDown/>
+              </Button>
+              <Button variant="contained" color="primary" className={classes.button}>
+                VAS Activation/Deactivations
+                <KeyboardArrowDown/>
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
-  );
+        </Card>
+        <ActionsBarOptions checked={true}/>
+      </div>
+    );
+  }
 }
 
 ActionsBar.propTypes = {
   classes: PropTypes.object.isRequired,
-  title: PropTypes.string.isRequired,
-  totalMails: PropTypes.number.isRequired,
-  totalClassfied: PropTypes.number.isRequired,
 };
 
 export default withStyles(styles)(ActionsBar);

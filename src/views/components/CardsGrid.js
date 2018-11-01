7c5984a8800b4204accd65500c0f6826d6fea21e
Vibhutha Kumarage <vkartz@gmail.com>
2018-11-01 21:41:12 +0530

    Added formData props to display values

--- src/views/components/CardsGrid.js
+++ src/views/components/CardsGrid.js
@@ -17,31 +17,31 @@ const styles = theme => ({
 });
 
 function CardsGrid(props) {
-  const { classes } = props;
+  const { classes, formData } = props;
 
   return (
     <div className={classes.root} style={{marginTop: '10px', marginLeft: '8px', marginRight: '8px'}}>
       <Grid container spacing={16}>
         <Grid item xs={12}>
-            <CustomCard2 title={'Usage'} background={'#33bfff'} totalMails={322} totalClassfied={121}/>
+            <CustomCard2 title={'Total'} background={'#33bfff'} totalMails={formData.TotalMails} totalClassfied={formData.TotalCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'Usage'} background={'#43a047'} totalMails={3322} totalClassfied={21}/>
+          <CustomCard title={'Usage'} background={'#43a047'} totalMails={formData.UsageMails} totalClassfied={formData.UsageCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'Recharge Card Related'} background={'#ff7da8'} totalMails={3122} totalClassfied={1321}/>
+          <CustomCard title={'Recharge Card Related'} background={'#ff7da8'} totalMails={formData.RechargeMails} totalClassfied={formData.RechargeCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'Genie'} background={'#ff3d00'} totalMails={3212} totalClassfied={2121}/>
+          <CustomCard title={'Genie'} background={'#ff3d00'} totalMails={formData.GenieMails} totalClassfied={formData.GenieCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'Billing'} background={'#ff9800'} totalMails={3422} totalClassfied={3321}/>
+          <CustomCard title={'Billing'} background={'#ff9800'} totalMails={formData.BillingMails} totalClassfied={formData.BillingCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'VAS Related'} background={'#d500f9'} totalMails={3122} totalClassfied={121}/>
+          <CustomCard title={'VAS Related'} background={'#d500f9'} totalMails={formData.VasMails} totalClassfied={formData.VasCorrect}/>
         </Grid>
         <Grid item xs={3}>
-          <CustomCard title={'Undecided'} background={'#9e9e9e'} totalMails={3122} totalClassfied={521}/>
+          <CustomCard title={'Undecided'} background={'#9e9e9e'} totalMails={formData.OtherMails} totalClassfied={formData.OtherCorrect}/>
         </Grid>
       </Grid>
     </div>
@@ -50,6 +50,7 @@ function CardsGrid(props) {
 
 CardsGrid.propTypes = {
   classes: PropTypes.object.isRequired,
+  formData: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(CardsGrid);

bfcb65969a1ad781f4248626606096d5eba9f060
Vibhutha Kumarage <vkartz@gmail.com>
2018-11-01 21:40:08 +0530

    Added states to DashBoard Component

--- src/views/pages/Dashboard/Dashboard.js
+++ src/views/pages/Dashboard/Dashboard.js
@@ -5,15 +5,35 @@ import CardsGrid from '../../components/CardsGrid';
 import NavigationTime from '../../components/NavigationTime';
 
 class Dashboard extends Component {
-    render() {
-      return (
-        <div className="App">
-          <PrimarySearchAppBar/>
-          <NavigationTime/>
-          <CardsGrid/>
-        </div>
-      );
+  constructor(props){
+    super(props)
+    this.state = {
+      TimePeriod: 0,
+      TotalMails: 0,
+      TotalCorrect: 0,
+      UsageMails: 0,
+      UsageCorrect: 0,
+      RechargeMails: 0,
+      RechargeCorrect: 0,
+      GenieMails: 0,
+      GenieCorrect: 0,
+      BillingMails: 0,
+      BillingCorrect: 0,
+      VasMails: 0,
+      VasCorrect: 0,
+      OtherMails: 0,
+      OtherCorrect: 0,
     }
+  }
+  render() {
+    return (
+      <div className="App">
+        <PrimarySearchAppBar/>
+        <NavigationTime/>
+        <CardsGrid formData={this.state}/>
+      </div>
+    );
+  }
   }
   
   export default Dashboard;

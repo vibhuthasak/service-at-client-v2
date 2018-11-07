9acca9c68271623819e65547bce26bf639401e44
Vibhutha Kumarage <vkartz@gmail.com>
2018-11-07 22:04:28 +0530

    Project Name changed

--- src/views/components/AppHeader.js
+++ src/views/components/AppHeader.js
@@ -51,7 +51,7 @@ class MenuAppBar extends React.Component {
         <AppBar position="fixed">
         <Toolbar>
             <Typography variant="h6" color="inherit" className={classes.grow}>
-              SERVICE @ by SIRA
+              SERVICE @ by Team undefined
             </Typography>
             <Link style={{ textDecoration: 'none' }} to="/">
               <Button variant="contained" style={{'background': "#00b0ff", "color": "white"}} className={classes.button}>

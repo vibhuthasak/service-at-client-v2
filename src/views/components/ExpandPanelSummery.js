1751477f7336be60b227b21e538ef19ccb5166c7
Vibhutha Kumarage <vkartz@gmail.com>
2018-11-14 17:47:51 +0530

    ExpandPanelSummery Component added

---
+++ src/views/components/ExpandPanelSummery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ExpandPanelSummery extends React.Component {
+
+  render() {
+    return <div>
+        {/* {this.props.id} */}
+        {/* {console.log(this.props.attachments)}
+        {console.log(this.props.feedback)} */}
+      </div>
+  }
+}
+
+ExpandPanelSummery.propTypes = {
+};
+
+export default ExpandPanelSummery;
